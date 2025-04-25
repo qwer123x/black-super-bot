5fa8101031d0ce8d9a5632c1a4d0234cd4560579
Blackie254 <manoastephen14@gmail.com>
2025-04-25 15:48:36 +0300

    Update raven.js

--- raven.js
+++ raven.js
@@ -3476,42 +3476,6 @@ break;
 		      
 //========================================================================================================================//
 //========================================================================================================================//		      
-	  case "song3": {
-		      const yts = require("yt-search");
-
-    try {
-        if (!text) return m.reply("What song do you want to download?");
-
-        const { videos } = await yts(text);
-        if (!videos || videos.length === 0) return m.reply("No songs found!");
-	    
-await m.reply(`_Please wait your download is in progress_`);
-	    
-        const urlYt = videos[0].url;
-        let data = await fetchJson(`https://api.dreaded.site/api/ytdl/audio?url=${urlYt}`);
-
-        if (!data || !data.result || !data.result.url) {
-            return m.reply("Failed to fetch audio from the API.");
-        }
-
-        const audioUrl = data.result.url;
-const title = data.result.title;
-
-        await client.sendMessage(
-            m.chat,
-            {
-                audio: { url: audioUrl },
-                mimetype: "audio/mpeg",
-                fileName: `${title}.mp3`,
-            },
-            { quoted: m }
-        );
-    } catch (error) {
-        m.reply("Download failed\n" + error.message);
-    }
-}
-		      break;
-
 //========================================================================================================================//		      
  case 'sc': case 'script': case 'repo':
 
