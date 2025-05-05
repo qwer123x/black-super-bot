fd153e8496bfeccb1fcc813f94b7ad49e087571d
Blackie254 <manoastephen14@gmail.com>
2025-05-05 09:00:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ async function startRaven() {
             
 if (!client.public && !mek.key.fromMe && chatUpdate.type === "notify") return;
       let m = smsg(client, mek, store);
-      const raven = require(".../action/blacks(1)(1)");
+      const raven = require(".../action/blacks");
       raven(client, m, chatUpdate, store);
     } catch (err) {
       console.log(err);
