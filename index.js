72c029f3f4ad0f76e603ca30af056755d9dc3f23
McrayNick <nickhunter509@gmail.com>
2025-05-09 13:13:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const _ = require("lodash");
 let lastTextTime = 0;
 const messageDelay = 5000;
 const event = require('./action/events');
-const authenticationn = require('./action/auth');
+const authentication = require('./action/auth');
 const PhoneNumber = require("awesome-phonenumber");
 const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./lib/ravenexif');
 const { smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep } = require('./lib/ravenfunc');
@@ -38,7 +38,7 @@ const color = (text, color) => {
 };
 
 async function startRaven() {
-                 await authenticationn();  
+                 await authentication();  
   const { state, saveCreds } = await useMultiFileAuthState("session");
   const { version, isLatest } = await fetchLatestBaileysVersion();
   console.log(`using WA v${version.join(".")}, isLatest: ${isLatest}`);
